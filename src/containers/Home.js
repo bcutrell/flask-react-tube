1aac7bb794aa609eac75a8dd4aed3370e7fc6fcd
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-09-21 13:04:41 -0400

    adds semantic ui

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
 
+// http://semantic-ui-forest.com/templates/bootstrap/navbar-fixed-top/
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1>Home</h1>
+                <Button>Click Here</Button>
             </div>
         )
     }
 }
 
-export default Home
+export default Home;
