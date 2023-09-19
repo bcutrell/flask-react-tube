65f3a77038ab791b2212ade803faf990e3912fff
Ben Cutrell <bcutrell@Apple-MacBook-Pro.local>
2023-09-19 16:16:19 -0400

    Starts App component update

--- bun-client/index.jsx
+++ bun-client/index.jsx
@@ -1,18 +1,32 @@
 import "react";
 import { renderToReadableStream } from "react-dom/server";
+import { Menu, Container } from 'semantic-ui-react'
 
-function MyComponent(props) {
-   return (
-     <body>
-       <h1 style={{ color: 'red' }}>{props.message}</h1>
-     </body>
+function App(props) {
+  return (
+    <div>
+      <link async rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"/>
+      <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+
+      <Menu borderless fixed={'top'}>
+          <Container>
+            <Menu.Item>
+              <a href="/">Home</a>
+            </Menu.Item>
+          </Container>
+      </Menu>
+
+    <Container>
+        <button className="ui button" role="button">Upload</button>
+    </Container>
+  </div>
    );
 }
 
 const server = Bun.serve({
     port: 3000,
     async fetch(request) {
-        const stream = await renderToReadableStream(<MyComponent message="Hello Bun!" />);
+        const stream = await renderToReadableStream(<App />);
         return new Response(stream, {
             headers: { "Content-Type": "text/html" },
         });
